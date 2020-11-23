5cf528b332e5c65cb63dc84ebd2ce76b8e012c1d
Samathingamajig <samathingamajig@gmail.com>
2020-11-23 01:14:35 -0600

    Added a delete button, and TONS of comments

--- src/components/QuizMaker/QuestionMaker/QuestionMaker.tsx
+++ src/components/QuizMaker/QuestionMaker/QuestionMaker.tsx
@@ -1,6 +1,8 @@
 import React, { ChangeEvent } from 'react';
 import ToolTip from '../../ToolTip/ToolTip';
 import './QuestionMaker.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 export interface QuestionMakerInterface {
   questionText: string;
@@ -12,30 +14,58 @@ export interface QuestionMakerProps {
   changeQuestionValues: (newValue: QuestionMakerInterface, index: number) => void;
   quizLength: number;
   changeQuestionIndex: (newIndex: number, prevIndex: number) => void;
+  deleteQuestion: (index: number) => () => void;
 }
 
-export default function QuestionMaker({ index, questionText, questionType, changeQuestionValues, quizLength, changeQuestionIndex }: QuestionMakerProps & QuestionMakerInterface) {
+export default function QuestionMaker({ index, questionText, questionType, changeQuestionValues, quizLength, changeQuestionIndex, deleteQuestion }: QuestionMakerProps & QuestionMakerInterface) {
   const setQuestionText = (newTextEvent: ChangeEvent<HTMLInputElement>) => {
+    // This function changes the text of the input element,
+    // and pushes it up to the parent's state changer function.
     changeQuestionValues({ questionText: newTextEvent.target.value, questionType }, index);
   };
 
   const setQuestionType = (newSelectionEvent: ChangeEvent<HTMLSelectElement>) => {
-    // console.log(newSelectionEvent);
-    // console.log(newSelectionEvent.target);
-    // console.log(newSelectionEvent.target.value);
+    // This function changes the question type of the select element,
+    // and pushes it up to the parent's state changer function.
     changeQuestionValues({ questionText, questionType: newSelectionEvent.target.value }, index);
   };
 
   const setQuestionIndex = () => {
-    let newIndex: string | null = prompt('What number should this question change to?');
+    // This function changes the index of the question in the parent's array.
+    // (This just changes the question number and ordering)
+
+    // Ask for what number this question should change to
+    let newIndex: string | null = window.prompt('What number should this question change to?');
+
+    // Validate the response...
     while (true) {
+      // If they press cancel, null is given, so we want to
+      // completely break out of this function.
       if (newIndex === null) return;
-      if (!/^-?\d+$/.test(newIndex + '')) newIndex = prompt(`Your input of "${newIndex || '<empty>'}" is invalid. Enter a new value, or select cancel.`);
-      else if (+newIndex <= 0 || +newIndex > quizLength) newIndex = prompt(`Your input of "${newIndex}" is outside of the range 1-${quizLength}. Enter a new value, or select cancel.`);
+
+      // Use a regex to test if it's not a number
+      /*
+       * !/^-?\d+$/
+       * !            Invert the output
+       *  /           Start the regex
+       *   ^          Pattern matches from the very beginning
+       *    -?        Optional negative sign, so 0 or 1 negative signs
+       *      \d+     \d means any digit 0-9, and the + means 1 or more, so 1 or more than 1 digits
+       *         $    Pattern matches to the very end
+       *          /   End the regex
+       */
+      if (!/^-?\d+$/.test(newIndex + '')) newIndex = window.prompt(`Your input of "${newIndex || '<empty>'}" is invalid. Enter a new value, or select cancel.`);
+      // So if it is a number, we need to make sure it's in the range.
+      // If it isn't, give an error and reprompt.
+      else if (+newIndex <= 0 || +newIndex > quizLength) newIndex = window.prompt(`Your input of "${newIndex}" is outside of the range 1-${quizLength}. Enter a new value, or select cancel.`);
+      // So if it is a number, and it's in range,
+      // then break out of the verification loop.
       else break;
     }
-    // console.log(newIndex);
 
+    // Pass this info to the parent's changeQuestionIndex function.
+    // We need to convert the number from a string into a number,
+    // add subtract 1 to make it 0-indexed.
     changeQuestionIndex(+newIndex - 1, index);
   };
 
@@ -55,6 +85,12 @@ export default function QuestionMaker({ index, questionText, questionType, chang
           <option value="yes-no">Yes or No</option>
         </select>
       </div>
+
+      <br />
+      <hr />
+      <button onClick={deleteQuestion(index)} className="RemoveQuestionMakerButton">
+        <FontAwesomeIcon icon={faTrashAlt} color="white" size="lg" className="RemoveQuestionMakerIcon"></FontAwesomeIcon>
+      </button>
     </div>
   );
 }
--- src/components/QuizMaker/QuizMaker.tsx
+++ src/components/QuizMaker/QuizMaker.tsx
@@ -1,31 +1,71 @@
 import React, { useState } from 'react';
 import QuestionMaker, { QuestionMakerInterface } from './QuestionMaker/QuestionMaker';
 import './QuizMaker.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function QuizMaker() {
   const [questionMakers, setQuestionMakers] = useState<QuestionMakerInterface[]>([]);
 
   const addQuestionMaker = () => {
+    // Add a blank QuestionMaker to the array of QuestionMakers
     setQuestionMakers((prev) => [...prev, { questionText: '', questionType: 'default' }]);
   };
 
   const changeQuestionValues = (newValue: QuestionMakerInterface, index: number): void => {
+    // This just changes the element at the specified index
+    // to the specified new value, but keep others the same.
+    //
+    // Loop through every element in the array of QuestionMakers.
+    // If the index is not the specified index, just return the original value.
+    // If the index is the specified index, then return the new value.
     setQuestionMakers((prev) => {
       return prev.map((prevValue, ind) => (ind !== index ? prevValue : newValue));
     });
   };
 
   const changeQuestionIndex = (newIndex: number, prevIndex: number): void => {
+    // This basically pops a QuestionMaker out of the array and inserts it
+    // at the new index that is specified.
+
     setQuestionMakers((prev) => {
+      // Get the original QuestionMaker
       const questionMaker = prev[prevIndex];
-      // console.log({ questionMaker, prev });
+
+      // Filter out the original QuestionMaker, so that all
+      // that you have left is everything except the given index.
       const newPrev = prev.filter((_, i) => i !== prevIndex);
-      // console.log({ newPrev });
-      const [start, end] = [newPrev.slice(0, newIndex), newPrev.slice(newIndex)];
-      // console.log({ start, questionMaker, end });
-      // console.log([...start, questionMaker, ...end]);
-      return [...start, questionMaker, ...end];
+
+      // Get everything that would be before the new slot,
+      // and everything that would be after the new slot.
+      const [before, after] = [newPrev.slice(0, newIndex), newPrev.slice(newIndex)];
+
+      // Return the new combined array, with the QuestionMaker
+      // inserted between the before part and the after part.
+      return [...before, questionMaker, ...after];
     });
+  };
+
+  const deleteQuestion = (index: number) => () => {
+    // This removes a question at the specified index,
+    // but asks for permission to do so.
+
+    // This function is curried, so the first call of this
+    // function sets up so internal state with the index
+    // so that we don't have pass in this function a
+    // weird way to the children element.
+
+    // First, ask for permission.
+    // We have to say "window.confirm" instead of just "confirm"
+    // because of an ESLint rule.
+    const shouldDelete: boolean = window.confirm('Are you sure you want to delete this question?\nThis cannot be undone!');
+
+    if (shouldDelete) {
+      // If we should delete, the filter out everything that's
+      // at the specified index.
+      // (This just keeps everything EXCEPT the element at the provided index)
+      setQuestionMakers((prev) => prev.filter((_, i) => i !== index));
+    }
   };
 
   return (
@@ -33,10 +73,12 @@ export default function QuizMaker() {
       <h1>QuizMaker</h1>
       <section id="QuestionMakerContainer">
         {questionMakers.map(({ questionText, questionType }, ind) => (
-          <QuestionMaker key={ind} index={ind} questionText={questionText} questionType={questionType} changeQuestionValues={changeQuestionValues} quizLength={questionMakers.length} changeQuestionIndex={changeQuestionIndex} />
+          <QuestionMaker key={ind} index={ind} questionText={questionText} questionType={questionType} changeQuestionValues={changeQuestionValues} quizLength={questionMakers.length} changeQuestionIndex={changeQuestionIndex} deleteQuestion={deleteQuestion} />
         ))}
       </section>
-      <button onClick={addQuestionMaker}>Add Question</button>
+      <button onClick={addQuestionMaker} className="AddQuestionMakerButton">
+        <FontAwesomeIcon icon={faPlus} className="AddQuestionMakerIcon" color="white" size="2x"></FontAwesomeIcon>
+      </button>
     </div>
   );
 }
